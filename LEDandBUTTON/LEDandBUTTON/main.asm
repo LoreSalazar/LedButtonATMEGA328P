998e271f55268a73fcf35b4df4d910a7277b7d09
LSL <lorena.salazar4444@gmail.com>
2025-01-12 12:35:05 -0600

    Encender led con switch

--- LEDandBUTTON/LEDandBUTTON/main.asm
+++ LEDandBUTTON/LEDandBUTTON/main.asm
@@ -5,25 +5,27 @@
 ; Author : LSL
 ;
 
-.org 0
 
-		LDI R17, (1 << DDC5)	;Se pone en alto el pin 5 del puerto C
-		OUT DDRC, R17			;Se configura como salida el pin 5 del puerto C
-		LDI R18, (0 << DDB0)	;Se configura en bajo el pin 0 del puerto B
-		OUT DDRB, R18			;Se configura como entrada el pin 5 del puerto C
+.org 0x00
 
-LOOP:
-		
-		;LDI R16, (1 << 0x05)    ;Se pone en 1 el registro R16 en la posición 5
-		;OUT PORTC, R16          ;Se asigna un 1 en el puerto C pin 5
-		
-		IN R19, PINB			;Un 0 o 1 en la posicion de PINB
-		
-		CPI R19, 1
-		BREQ true
+		LDI R16, 0b0010_0000	;Se pone en alto el pin 5 del puerto C
+		OUT DDRC, R16			;Se configura como salida el pin 5 del puerto C
+		LDI R16, 0b0000_0000	;Se configura en bajo el pin 0 del puerto B
+		OUT DDRB, R16			;Se configura como entrada el pin 0 del puerto B
+
+LOOP:		
+		IN R16, PINB			;Se lee el estado del PINB
+		LDI R17, 0b0000_0001	;Iguala a 1 el registro 17
+		CP R16, R17				;Compara si es igual el regitro 16 al registro 17
+		BREQ Led_On				;Si es igual va a la etiqueta de led on
+		BRNE Led_Off			;Si no es igual se va a la etiqueta de led off
+
+Led_On:
+		LDI R16, 0b0010_0000
+		OUT PORTC, R16
 		RJMP LOOP
-
-		true:
-		LDI R16, (1 << 0x05)    ;Se pone en 1 el registro R16 en la posición 5
-		OUT PORTC, R16          ;Se asigna un 1 en el puerto C pin 5
+Led_Off:
+		LDI R16, 0b0000_0000
+		OUT PORTC, R16
+		RJMP LOOP
 
